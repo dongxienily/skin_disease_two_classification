8ab3309c75d13acc908e8cfe46bf515dec6caf12
dongxienily <46030136+dongxienily@users.noreply.github.com>
2020-04-03 10:48:44 +0800

    Update args.py

--- skin_two_classification/args.py
+++ skin_two_classification/args.py
@@ -18,7 +18,7 @@ parser.add_argument('-test_txt_path',type=str,default='./dataset/test.txt')
 parser.add_argument('-val_txt_path',type=str,default='./dataset/val.txt')
 
 # optimizer
-parser.add_argument('--optimizer',default='sgd',choices=['sgd','rmsprop','adam','radam'])
+parser.add_argument('--optimizer',default='adam',choices=['sgd','rmsprop','adam','radam'])
 parser.add_argument("--lr",type=float,default=0.001)
 parser.add_argument('--lr-fc-times', '--lft', default=5, type=int,
                     metavar='LR', help='initial model last layer rate')
@@ -41,10 +41,10 @@ parser.add_argument("--checkpoint",type=str,default='./checkpoints')
 parser.add_argument("--resume",default='',type=str,metavar='PATH',help='path to save the latest checkpoint')
 parser.add_argument("--batch_size",type=int,default=16)
 parser.add_argument("--start_epoch",default=0,type=int,metavar='N')
-parser.add_argument('--epochs',default=30,type=int,metavar='N')
+parser.add_argument('--epochs',default=200,type=int,metavar='N')
 
 
-parser.add_argument('--image-size',type=int,default=288)
+parser.add_argument('--image-size',type=int,default=299)
 parser.add_argument('--arch',default='resnet50',choices=['resnet34','resnet18','resnet50'])
 parser.add_argument('--num_classes',default=2,type=int)
 
